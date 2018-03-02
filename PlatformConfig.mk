836eab6bc658dfca8e5ed6c7b66ab6d127e4d892
David <Davidteri91@gmail.com>
2018-03-03 00:36:39 +0100

    nile: remove extra folders
    this is set by common repo
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -97,7 +97,6 @@ TARGET_NO_RECOVERY := true
 BOARD_USES_RECOVERY_AS_BOOT := true
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
-BOARD_ROOT_EXTRA_FOLDERS := bt_firmware firmware firmware/image persist
 BOARD_ROOT_EXTRA_SYMLINKS := /vendor/lib/dsp:/dsp
 
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
