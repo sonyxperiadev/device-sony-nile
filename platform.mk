a2081f1a56003db9f31eac2d1c46c396b117be87
Alin Jerpelea <alin.jerpelea@sony.com>
2018-05-14 19:31:30 +0200

    add audio_tuning_mixer.txt
    the new audio HAL uses audio_tuning_mixer.txt for calibration
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- platform.mk
+++ platform.mk
@@ -35,7 +35,7 @@ AB_OTA_PARTITIONS += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(SONY_ROOT)/vendor/etc/aanc_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/aanc_tuning_mixer.txt \
+    $(SONY_ROOT)/vendor/etc/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
     $(SONY_ROOT)/vendor/etc/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
 
 # Media
