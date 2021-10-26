041afbad31551dc5b06060d7eb46c752ae1547c8
Pavel Dubrova <pashadubrova@gmail.com>
2021-10-26 04:56:38 +0300

    platform: Disable camera dual isp sync
    SDM660 platform does not support camera dual isp sync.
    
    Setting this property allows us to avoid introducing
    workrounds on the kernel side as it was done in the
    4.14 kernel.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- platform.mk
+++ platform.mk
@@ -219,6 +219,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.qcom.bluetooth.soc=cherokee
 
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.camera.dual.isp.sync=0
+
 # Fluence
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.audio.fluencetype=fluence
